ecbe7982603ec96fed8399f0917593148a18ec0e
dongheonchoi <124031425+dongheonchoi@users.noreply.github.com>
2023-02-27 23:30:48 +0900

    2/27 프로그래머스
    같은 숫자는 싫어

---
+++ programmersStackQueue01.java
@@ -0,0 +1,18 @@
+package practice;
+
+import java.util.*;
+
+public class programmersStackQueue01 {
+	public int[] solution(int []arr) {
+        ArrayList<Integer> answerList = new ArrayList<Integer>();
+        //
+        int value = -1;
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] != value) {
+                answerList.add(arr[i]);
+                value = arr[i];
+            }
+        }
+        return answerList.stream().mapToInt(i->i).toArray();
+    }
+}
